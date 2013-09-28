3a9e957dad8c9fef5627173463334d56a0cc408a
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-28 18:21:55 +0200

    Update version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class omnigraffle {
   package { 'OmniGraffle':
     provider => 'appdmg_eula',
-    source   => 'http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniGraffle-5.4.3.dmg'
+    source   => 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniGraffle-6.0.dmg'
   }
 }
--- spec/classes/omnigraffle_spec.rb
+++ spec/classes/omnigraffle_spec.rb
@@ -4,6 +4,6 @@ describe 'omnigraffle' do
 
   it { should contain_class('omnigraffle') }
   it { should contain_package('OmniGraffle').with_provider('appdmg_eula') }
-  it { should contain_package('OmniGraffle').with_source('http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniGraffle-5.4.3.dmg') }
+  it { should contain_package('OmniGraffle').with_source('http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniGraffle-6.0.dmg') }
 
 end
